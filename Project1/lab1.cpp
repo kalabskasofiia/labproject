714a86cb5e58bf0d8420752f1d18cf467b9c0d11
Mariia <m100liar@ukr.net>
2025-09-25 12:21:21 +0300

    lab1 cpp

--- Project1/lab1.cpp
+++ Project1/lab1.cpp
@@ -1,1 +1,14 @@
 #include "lab1.h"
+#include <stdexcept>
+
+WeightedRandom::WeightedRandom(const std::vector<int>& v, const std::vector<int>& w)
+	: values(v), weights(w), gen(std::random_device{}()), dist(w.begin(), w.end()) {
+	if (v.size() != w.size() || v.empty()) {
+		throw std::invalid_argument("Некоректні вхідні дані");
+	}
+}
+
+int WeightedRandom::operator()() {
+	ind idx = dist(gen);
+	return values[idx];
+}
